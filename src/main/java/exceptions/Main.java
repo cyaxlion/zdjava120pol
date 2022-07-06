e731d642a64d958d1774c955c93f5bf4cb81c283
cyaxlion <kjanus@dozp.pl>
2022-07-06 19:18:07 +0200

    Exceptions

---
+++ src/main/java/exceptions/Main.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Input inputter = new Input();
+        System.out.println("Liczba: " + inputter.readNumber());
+    }
+}
